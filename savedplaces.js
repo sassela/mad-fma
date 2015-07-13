9bd58db9d64c9f31eda33f03ae8efbe1d2f13c2e
= <sassela@live.com>
2015-07-13 07:03:27 +0100

    Add styles

--- savedplaces.js
+++ savedplaces.js
@@ -13,8 +13,8 @@ $(document).on("pagecontainerbeforeshow", function (e, ui) {
 function storeData() {
 	var placeDetails = [
     {name:'place1', image:"images/places/house1b.png", price:650, url:'index.html'},
-    {name:'place2', image:"images/places/house1b.png", price:650, url:'index.html'},
-    {name:'place3', image:"images/places/house1b.png", price:650, url:'index.html'}
+    {name:'place2', image:"images/places/house2.png", price:650, url:'index.html'},
+    {name:'place3', image:"images/places/house3c.png", price:650, url:'index.html'}
     ];
 	localStorage.setItem("placeDetails",JSON.stringify(placeDetails));
 }
@@ -25,10 +25,12 @@ function getData() {
 }
 
 function displayDetails(details) {
-	var text = '';
+	var text = '<ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow">';
+
 	for(var i in details) {
-		text += i + ': ' + details[i].name + '<br>';
+		text += '<li class="ui-li-has-thumb ui-first-child"><a href=' + details[i].url + ' rel ="external" class="ui-btn ui-btn-icon-right ui-icon-caret-r" ><img src=' + details[i].image + '><h2>Z' + details[i].price + ' / month</h2><p>' + details[i].name + '</p></a></li>';
 	}
+	text += '</ul>';
 	$("#result").html(text);
 }
 
