5b4cd5dec225c6b7c52cbc4230e72aaec7ab26c3
= <sassela@live.com>
2015-08-03 12:45:18 +0100

    Refactor: extendBounds, setPointB

--- src/js/maps.js
+++ src/js/maps.js
@@ -22,32 +22,32 @@ var map;
 //  population: 603502
 //};
 
+var bounds = new google.maps.LatLngBounds();
+
 function initialize() {
+	// dynamically update for each place
+	var pointB = new google.maps.LatLng(51.52307, -0.12426);
 
 	var mapOptions = {
 	 zoom: 12
 	};
+
 	map = new google.maps.Map(document.getElementById('map-canvas'),
 	           mapOptions);
 
-	getPointA();
+	setPointA();
+	setPointB(pointB);
 }
 
-function getPointA(){
-	var pos;
-	var accomm1;
+function setPointA(){
+	var pointA;
 // Try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      pos = new google.maps.LatLng(position.coords.latitude,
+      pointA = new google.maps.LatLng(position.coords.latitude,
                                        position.coords.longitude);
-			accomm1 = new google.maps.LatLng(51.52307, -0.12426);
-
-			placePointAMarker(pos);
-
-			placePointBMarker(accomm1);
-
-      fitBounds(pos, accomm1);
+			placePointAMarker(pointA);
+			extendBounds(bounds, pointA);
 
 //	  todo update with accomm info
 //      var infowindow = new google.maps.InfoWindow({
@@ -65,6 +65,11 @@ function getPointA(){
   }
 }
 
+function setPointB(pointB){
+	placePointBMarker(pointB);
+	extendBounds(bounds, pointB);
+}
+
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
     var content = 'Error: The Geolocation service failed.';
@@ -72,7 +77,7 @@ function handleNoGeolocation(errorFlag) {
     var content = 'Error: Your browser doesn\'t support geolocation.';
   }
 
-	var pos = new google.maps.LatLng(51.521951, -0.130204);
+	var defaultPointA = new google.maps.LatLng(51.521951, -0.130204);
 
   var options = {
 //    map: map,
@@ -80,11 +85,7 @@ function handleNoGeolocation(errorFlag) {
     content: content
   };
 
-  var marker = new google.maps.Marker({
-	    position: pos,
-	    map: map,
-	    title: 'You are here'
-	});
+	placePointAMarker(defaultPointA);
 
 //	todo update
 //  var infowindow = new google.maps.InfoWindow(options);
@@ -92,9 +93,9 @@ function handleNoGeolocation(errorFlag) {
 
 }
 
-function placePointAMarker(pointa){
+function placePointAMarker(pointA){
   var marker = new google.maps.Marker({
-      position: pointa,
+      position: pointA,
       map: map,
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
@@ -105,22 +106,17 @@ function placePointAMarker(pointa){
     });
 }
 
-function placePointBMarker(pointb){
+function placePointBMarker(pointB){
 	var marker = new google.maps.Marker({
-	    position: pointb,
+	    position: pointB,
 	    map: map,
 	    title: 'Accomm 1'
 	  });
 }
 
-function fitBounds(pointa, pointb) {
-//fits map bound to include both points
-        var LatLngList = new Array (pointa, pointb);
-	        var bounds = new google.maps.LatLngBounds ();
-	        for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
-	          bounds.extend (LatLngList[i]);
-        }
-        map.fitBounds(bounds);
+function extendBounds(bounds, point) {
+	bounds.extend(point);
+	map.fitBounds(bounds);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
