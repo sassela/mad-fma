2aabe05a7110014c408688e0c248471a55161917
= <sassela@live.com>
2015-08-04 07:04:19 +0100

    Refactor item local storage

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -6,25 +6,14 @@ $(document).on("pagecontainerbeforeshow", function (e, ui) {
 	}
 });
 
-// TODO refactor
-function savePlace(key, place) {
-	var savedPlaces = JSON.parse( localStorage.getItem('savedPlaces'));
-	if(savedPlaces){
-		if(savedPlaces[key] === null) {
-			savedPlaces.push(place);
+function storeItem(lsItemStr, key, place) {
+	var storedItems = JSON.parse( localStorage.getItem(lsItemStr));
+	if(storedItems){
+		if(storedItems[key] === null) {
+			storedItems.push(place);
 		}
-	} else savedPlaces = place;
-	localStorage.setItem("savedPlaces",JSON.stringify(savedPlaces));
-}
-
-function reservePlace(key, place) {
-	var reservedPlaces = JSON.parse( localStorage.getItem('reservedPlaces'));
-	if(reservedPlaces){
-		if(reservedPlaces[key] === null){
-			reservedPlaces.push(place);
-		}
-	} else reservedPlaces = place;
-	localStorage.setItem("reservedPlaces",JSON.stringify(reservedPlaces));
+	} else storedItems = place;
+	localStorage.setItem(lsItemStr,JSON.stringify(storedItems));
 }
 
 function getPage() {
@@ -36,16 +25,17 @@ function initLocalStorage() {
 	// create place object using page id as key
   var key = $.mobile.pageContainer.pagecontainer('getActivePage').attr( 'id' );
   var place = {};
+  // todo add reservation dates to reserved property. Add stars to favourites?
   place[key]={name:$("#place").text(), image:$('img#image ').attr('src'), price:$("#price").text(), url:getPage()};
 
 	// save property event (add to favourites)
 	$("#save").click(function() {
-		savePlace(key, place);
+		storeItem('savedPlaces', key, place);
 	});
 
 	// reserve property event
 	$("#reserve").click(function() {
-    reservePlace(key, place);
+		storeItem('reservedPlaces', key, place);
   });
 }
 
