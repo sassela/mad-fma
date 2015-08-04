355ba9d8c14884119826a4b424741e74cd0c20de
= <sassela@live.com>
2015-08-04 09:19:28 +0100

    Add images array

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,15 +1,24 @@
 var currentImageEl = 0;
 
 function init() {
+	var key = $.mobile.pageContainer.pagecontainer('getActivePage').attr( 'id' );
+  var placeImages = {
+  	"place1-detail": ["images/places/house1.jpeg", "images/places/house1b.jpeg", "images/places/house1c.jpeg", "images/places/house1d.jpeg"],
+  	// todo change page keys
+  	002: ["images/places/house2.jpeg", "images/places/house2b.jpeg", "images/places/house2c.jpeg"],
+  	003: ["images/places/house3.jpeg", "images/places/house3b.jpeg", "images/places/house3c.jpeg"]
+  };
+  // TODO add horizontal / vertical attribute. Alt text.
+
 	$("img").click(function() {
-		navigate(placeImages, currentImageEl, 1);
+		navigate(placeImages[key], currentImageEl, 1);
 	});
-	// TODO add swipe
+
 	$("#next").click(function() {
-		navigate(placeImages, currentImageEl, 1);
+		navigate(placeImages[key], currentImageEl, 1);
 	});
 	$( document ).on( "swipeleft", function() {
-    	navigate(placeImages, currentImageEl, 1);
+    	navigate(placeImages[key], currentImageEl, 1);
     });
 
 	$("#prev").click(function() {
@@ -18,11 +27,11 @@ function init() {
     $( document ).on( "swiperight", function() {
     	navigate(placeImages, currentImageEl, -1);
     });
-
 }
 
-var placeImages = ["images/places/house1.jpeg", "images/places/house1b.jpeg", "images/places/house1c.jpeg", "images/places/house1d.jpeg"];
-// TODO all images object. add horizontal / vertical attribute. Alt text.
+function display(images) {
+	console.log(images[0]);
+}
 
 function navigate(placeImages, current, direction) {
 	if((current + direction) > placeImages.length){
@@ -36,7 +45,7 @@ function navigate(placeImages, current, direction) {
 	currentImageEl = current;
 }
 
-$( document ).on( "pagecreate", function() {
+$( document ).on( "pageshow", function() {
 	init();
     $( ".photopopup" ).on({
         popupbeforeposition: function() {
