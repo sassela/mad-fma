bf89dae1167627d10d502f305bc32fd13ddd3d65
= <sassela@live.com>
2015-08-04 11:58:49 +0100

    Add page details

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -4,9 +4,8 @@ function init() {
 	var key = $.mobile.pageContainer.pagecontainer('getActivePage').attr( 'id' );
   var placeImages = {
   	"place1-detail": ["images/places/house1.jpeg", "images/places/house1b.jpeg", "images/places/house1c.jpeg", "images/places/house1d.jpeg"],
-  	// todo change page keys
-  	002: ["images/places/house2.jpeg", "images/places/house2b.jpeg", "images/places/house2c.jpeg"],
-  	003: ["images/places/house3.jpeg", "images/places/house3b.jpeg", "images/places/house3c.jpeg"]
+  	"place2-detail": ["images/places/house2.jpeg", "images/places/house2b.jpeg", "images/places/house2c.jpeg"],
+  	"place3-detail": ["images/places/house3.jpeg", "images/places/house3b.jpeg", "images/places/house3c.jpeg"]
   };
   // TODO add horizontal / vertical attribute. Alt text.
 
