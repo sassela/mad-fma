35f92809aa69bdb82e27aadc18fde7f8409cbb51
= <sassela@live.com>
2015-08-04 10:10:47 +0100

    Update save & reserve pages in event no places are saved

--- src/js/reservedplaces.js
+++ src/js/reservedplaces.js
@@ -15,12 +15,15 @@ function getData() {
 }
 
 function displayDetails(details) {
-	var text = '<ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow">';
+	var text;
+	if(details){
+		 text = '<ul data-role="listview" data-inset="true" class="ui-listview ui-listview-inset ui-corner-all ui-shadow">';
 
-	for(var i in details) {
-		text += '<li class="ui-li-has-thumb ui-first-child"><a href=\"' + details[i].url + '.html\" rel ="external" class="ui-btn ui-btn-icon-right ui-icon-caret-r" ><img src=' + details[i].image + '><h2>' + details[i].price + '</h2><p>' + details[i].name + '</p></a></li>';
-	}
-	text += '</ul>';
+  	for(var i in details) {
+  		text += '<li class="ui-li-has-thumb ui-first-child"><a href=\"' + details[i].url + '.html\" rel ="external" class="ui-btn ui-btn-icon-right ui-icon-caret-r" ><img src=' + details[i].image + '><h2>' + details[i].price + '</h2><p>' + details[i].name + '</p></a></li>';
+  	}
+  	text += '</ul>';
+	} else text = '<p>You don\'t have any places reserved. <a href="index.html" rel="external">Search</a> to reserve a place.</p>';
 	$("#result").html(text);
 }
 
